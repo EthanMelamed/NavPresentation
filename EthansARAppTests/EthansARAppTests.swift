25d46db61c5a997e5be2d34d345c862bc0b42084
Ethan <melamee@navcanada.ca>
2017-08-05 15:25:29 -0400

    Initial Commit

---
+++ EthansARAppTests/EthansARAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  EthansARAppTests.swift
+//  EthansARAppTests
+//
+//  Created by Ethan  on 2017-08-05.
+//  Copyright © 2017 Ethan . All rights reserved.
+//
+
+import XCTest
+@testable import EthansARApp
+
+class EthansARAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
